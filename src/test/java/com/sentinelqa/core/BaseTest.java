7bcc7e5d488692c103097999971b4431a15119dc
Pawan Joshi <pawanjoshi023@gmail.com>
2026-06-27 13:18:16 +0530

    refactor(core): Use BrowserContext for better test isolation

--- src/test/java/com/sentinelqa/core/BaseTest.java
+++ src/test/java/com/sentinelqa/core/BaseTest.java
@@ -1,6 +1,7 @@
 package com.sentinelqa.core;
 
 import com.microsoft.playwright.Browser;
+import com.microsoft.playwright.BrowserContext;
 import com.microsoft.playwright.Page;
 import com.microsoft.playwright.Playwright;
 import com.sentinelqa.driver.BrowserFactory;
@@ -12,20 +13,24 @@ public class BaseTest {
 
     protected Playwright playwright;
     protected Browser browser;
+    protected BrowserContext context;
     protected Page page;
 
     @BeforeEach
     void setUp() {
         playwright = Playwright.create();
         browser = BrowserFactory.createBrowser(playwright, BrowserType.CHROME);
-        page = browser.newPage();
+        context = browser.newContext();
+        page = context.newPage();
     }
 
     @AfterEach
     void tearDown() {
+        // Closing the browser will close all its contexts and pages.
         if (browser != null) {
             browser.close();
         }
+        // Finally, close the Playwright instance.
         if (playwright != null) {
             playwright.close();
         }
